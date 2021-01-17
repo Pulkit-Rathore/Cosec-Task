19e4e0cfe45b06d48df33cb8830bddd01e7954ec
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2021-01-17 12:19:36 +0530

    Add files via upload

---
+++ Cosec Task/counting with half pyramid row.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int r;
+    cout<<"how many rows you want";
+    cin>>r;
+    for(int i=1; i<=r; i++){
+        for(int j=1; j<=i; j++){
+
+            if(r>1){
+                cout<<j;
+            }
+            else{
+                cout<<i;
+            }
+        }
+
+        cout<<endl;
+    }
+}
